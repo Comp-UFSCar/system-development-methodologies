6f55d665be2f3140e2995c3faf076b2e3c2447ce
Lucas David <lucasolivdavid@gmail.com>
2014-09-12 00:09:31 -0300

    Implement Search method in EmployeeService class

--- Gamedalf.Services/EmployeeService.cs
+++ Gamedalf.Services/EmployeeService.cs
@@ -1,10 +1,29 @@
 ﻿using Gamedalf.Core.Data;
 using Gamedalf.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
 
 namespace Gamedalf.Services
 {
     public class EmployeeService : Service<Employee>
     {
         public EmployeeService(ApplicationDbContext db) : base(db) { }
+
+        public async Task<ICollection<Employee>> Search(string q = null)
+        {
+            if (String.IsNullOrEmpty(q))
+            {
+                return await All();
+            }
+
+            return await db.Employees
+                .Where(e => e.Email.Contains(q))
+                .OrderBy(e => e.Email)
+                .ToListAsync();
+        }
     }
 }
