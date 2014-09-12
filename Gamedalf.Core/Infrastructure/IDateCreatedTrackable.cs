9d9e497e1cfd6ea3c7906ee715dd96d5837c1b69
Lucas David <lucasolivdavid@gmail.com>
2014-09-12 10:45:10 -0300

    Update IDateCreatedTrackable.cs

---
+++ Gamedalf.Core/Infrastructure/IDateCreatedTrackable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gamedalf.Core.Infrastructure
+{
+    /// <summary>
+    /// Define attribute DateCreated
+    /// </summary>
+    public interface IDateCreatedTrackable
+    {
+        DateTime DateCreated { get; set; }
+    }
+}
--- Gamedalf/Controllers/EmployeesController.cs
+++ Gamedalf/Controllers/EmployeesController.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using Gamedalf.Core.Data;
 using Gamedalf.Core.Models;
 using Gamedalf.ViewModels;
 using Gamedalf.Services;
