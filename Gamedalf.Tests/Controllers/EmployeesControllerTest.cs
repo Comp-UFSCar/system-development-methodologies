e08439d1b97d01de2656818b5db836f5d2e7d346
Lucas David <lucasolivdavid@gmail.com>
2014-09-12 23:53:18 -0300

    Update #4, create EmployeesControllerTest

---
+++ Gamedalf.Tests/Controllers/EmployeesControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gamedalf.Services;
+using Moq;
+using System.Collections.Generic;
+using Gamedalf.Core.Models;
+using Gamedalf.Controllers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Gamedalf.Tests.Controllers
+{
+    [TestClass]
+    public class EmployeesControllerTest
+    {
+        private Mock<EmployeeService> employees;
+        private Mock<ApplicationUserManager> userManager;
+
+        [TestInitialize]
+        public void setup()
+        {
+            employees = new Mock<EmployeeService>();
+            userManager = new Mock<ApplicationUserManager>();
+        }
+
+        [TestMethod]
+        public async Task EmployeesControllerIndex()
+        {
+            var controller = new EmployeesController(userManager.Object, employees.Object);
+
+            var result = (await controller.Index()) as ViewResult;
+
+            Assert.AreEqual(4, controller.ViewData.Count);
+        }
+    }
+}
