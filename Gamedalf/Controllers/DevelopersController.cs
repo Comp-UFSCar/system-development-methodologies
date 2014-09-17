07431fdfd4d1419e16df079c1353e208c876ed72
Lucas David <lucasolivdavid@gmail.com>
2014-09-17 13:00:23 -0300

    Fix #25

---
+++ Gamedalf/Controllers/DevelopersController.cs
@@ -0,0 +1,123 @@
+﻿using Gamedalf.Core.Models;
+using Gamedalf.Services;
+using Gamedalf.ViewModels;
+using PagedList;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace Gamedalf.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        private readonly DeveloperService         developers;
+        private readonly PlayerService            players;
+        private readonly ApplicationUserManager   UserManager;
+        private          ApplicationSignInManager _signInManager;
+        public ApplicationSignInManager           SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set { _signInManager = value; }
+        }
+
+        public DevelopersController(ApplicationUserManager userManager, DeveloperService _developers, PlayerService _players)
+        {
+            UserManager = userManager;
+            developers  = _developers;
+            players     = _players;
+        }
+
+        public DevelopersController(ApplicationUserManager userManager, ApplicationSignInManager signInManager, DeveloperService _developers, PlayerService _players)
+        {
+            UserManager   = userManager;
+            SignInManager = signInManager;
+            developers    = _developers;
+            players       = _players;
+        }
+
+        // GET: Developers
+        public async Task<ActionResult> Index(string q = null, int page = 1, int size = 10)
+        {
+            ViewBag.q = q;
+
+            var list = (await developers.Search(q))
+                .ToPagedList(page, size);
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_List", list);
+            }
+
+            return View(list);
+        }
+
+        // GET: Developers/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Developer developer = await developers.Find(id);
+            if (developer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(developer);
+        }
+
+        //
+        // GET: Developers/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(DeveloperRegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var player    = await players.Find(model.Id);
+            var developer = await developers.Register(player);
+            var result    = await UserManager.AddToRoleAsync(developer.Id, "developer");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                developers.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Gamedalf/ViewModels/DeveloperViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamedalf.ViewModels
+{
+    public class DeveloperRegisterViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public bool AcceptTerms { get; set; }
+    }
+}
