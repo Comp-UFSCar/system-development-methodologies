6fa461bf7845873f1a3c2a57142e7575ca64190b
Lucas David <lucasolivdavid@gmail.com>
2014-09-17 12:59:01 -0300

    Fix #22

---
+++ Gamedalf.Core/Models/Developer.cs
@@ -0,0 +1,9 @@
+﻿using Gamedalf.Core.Infrastructure;
+
+namespace Gamedalf.Core.Models
+{
+    public class Developer : Player
+    {
+        //public virtual ICollection<Game> Games { get; set; }
+    }
+}
