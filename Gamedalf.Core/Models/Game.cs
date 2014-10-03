70ac334ffae6acc5ed0e51b96625dddae7308b4c
Lucas David <lucasolivdavid@gmail.com>
2014-10-03 15:38:53 -0300

    Fix #12

---
+++ Gamedalf.Core/Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gamedalf.Core.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public decimal? Price { get; set; }
+
+        [ScaffoldColumn(false)]
+        [ForeignKey("Employee")]
+        public string EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        [ScaffoldColumn(false)]
+        [ForeignKey("Developer")]
+        public string DeveloperId { get; set; }
+        public virtual Developer Developer { get; set; }
+    }
+}
