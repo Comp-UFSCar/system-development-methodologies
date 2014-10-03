5aed71e14a9de66eb537c4f961a10d0df2adf78e
Lucas David <lucasolivdavid@gmail.com>
2014-10-03 14:58:09 -0300

    Update #9

---
+++ Gamedalf.Tests/Testdata/DevelopersTestData.cs
@@ -0,0 +1,42 @@
+﻿using Gamedalf.Core.Models;
+using Gamedalf.Tests.Infrastructure;
+using System.Collections.Generic;
+
+namespace Gamedalf.Tests.Testdata
+{
+    public class DevelopersTestData : ITestData<Developer>
+    {
+        public ICollection<Developer> Data { get; set; }
+
+        public DevelopersTestData()
+        {
+            Data = new List<Developer>
+            {
+                new Developer 
+                {
+                    Id       = "developer1",
+                    Email    = "lucasolivdavid@gmail.com",
+                    UserName = "lucasolivdavid@gmail.com",
+                },
+                new Developer
+                {
+                    Id       = "developer2",
+                    Email    = "maria@db.net",
+                    UserName = "maria@db.net",
+                },
+                new Developer
+                {
+                    Id       = "developer3",
+                    Email    = "johnhall@provider.com",
+                    UserName = "johnhall@provider.com"
+                },
+                new Developer
+                {
+                    Id       = "developer4",
+                    Email    = "email@mail.com",
+                    UserName = "email@mail.com"
+                }
+            };
+        }
+    }
+}
