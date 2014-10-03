c49baeed88e3bc93747d396a2e9db636289b4a78
Lucas David <lucasolivdavid@gmail.com>
2014-10-03 15:39:27 -0300

    Update #

---
+++ Gamedalf.Tests/Testdata/GamesTestData.cs
@@ -0,0 +1,39 @@
+﻿using Gamedalf.Core.Models;
+using Gamedalf.Tests.Infrastructure;
+using System.Collections.Generic;
+
+namespace Gamedalf.Tests.Testdata
+{
+    public class GamesTestData : ITestData<Game>
+    {
+        public ICollection<Game> Data { get; set; }
+
+        public GamesTestData()
+        {
+            Data = new List<Game>
+            {
+                new Game 
+                {
+                    Id = 1,
+                    Price = 1,
+                    Title = "Minecraft",
+                    DeveloperId = "developer1"
+                },
+                new Game
+                {
+                    Id = 2,
+                    Price = 2,
+                    Title = "Warcraft",
+                    DeveloperId = "developer2"
+                },
+                new Game
+                {
+                    Id = 3,
+                    Price = 2,
+                    Title = "Assassin's Creed",
+                    DeveloperId = "developer3"
+                },
+            };
+        }
+    }
+}
