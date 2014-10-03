a75d2bbf2151e9cb1fd33455fcdf3e0764f88bc6
Lucas David <lucasolivdavid@gmail.com>
2014-10-03 17:28:20 -0300

    Fix #32

---
+++ Gamedalf.Services/GameService.cs
@@ -0,0 +1,28 @@
+﻿using Gamedalf.Core.Data;
+using Gamedalf.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gamedalf.Services
+{
+    public class GameService : Service<Game>
+    {
+        public GameService(ApplicationDbContext _db) : base(_db) { }
+
+        public virtual async Task<ICollection<Game>> Search(string q)
+        {
+            if (String.IsNullOrEmpty(q))
+            {
+                return await All();
+            }
+
+            return await db.Games
+                .Where(e => e.Title.Contains(q))
+                .OrderBy(e => e.Title)
+                .ToListAsync();
+        }
+    }
+}
