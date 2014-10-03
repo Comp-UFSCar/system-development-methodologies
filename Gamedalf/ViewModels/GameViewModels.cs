6677e509cc1fb398437f89d6a9f08d129d15b185
Lucas David <lucasolivdavid@gmail.com>
2014-10-03 17:30:00 -0300

    Implement GameCreateViewModel and GameEditViewModel

---
+++ Gamedalf/ViewModels/GameViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamedalf.ViewModels
+{
+    public class GameCreateViewModel
+    {
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Price")]
+        public decimal? Price { get; set; }
+    }
+
+    public class GameEditViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
