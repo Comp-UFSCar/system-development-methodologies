4441457849f9698e4da8b128ee8edb509fc99791
Lucas David <lucasolivdavid@gmail.com>
2014-10-15 22:05:20 -0300

    Fix #37

---
+++ Gamedalf.Services/TermsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Gamedalf.Core.Data;
+using Gamedalf.Core.Models;
+using Gamedalf.Services.Infrastructure;
+
+namespace Gamedalf.Services
+{
+    public class TermsService : Service<Terms>
+    {
+        public TermsService(ApplicationDbContext db) : base(db) { }
+
+        public virtual async Task<ICollection<Terms>> Search(string q)
+        {
+            if (String.IsNullOrEmpty(q))
+            {
+                return await All();
+            }
+
+            return await Db.Terms
+                .Where(t => t.Title.Contains(q) || t.Employee.Email.Contains(q))
+                .Include(t => t.Employee)
+                .OrderBy(e => e.Title)
+                .ThenByDescending(e => e.DateCreated)
+                .ToListAsync();
+        }
+
+        public virtual async Task<Terms> Latest(string q)
+        {
+            // Considering Search(q) has arranged them decreasingly, the first
+            // element represents the one closest to the current date.
+            return (await Search(q)).First();
+        }
+
+        public virtual async Task<Terms> TermsOnDate(string q, DateTime date)
+        {
+            // Gets the first of all terms that have their DateCreated < date.
+            // Considering Search(q) has arranged them decreasingly, the first
+            // element represents the one closest to date.
+            return (await Search(q))
+                .Where(t => t.DateCreated < date)
+                .First();
+        }
+    }
+}
