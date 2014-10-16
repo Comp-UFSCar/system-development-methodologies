05fbb338ea15b884912b6a0ff5f758ebddb0fcb3
Lucas David <lucasolivdavid@gmail.com>
2014-10-15 21:40:44 -0300

    Fix #35

---
+++ Gamedalf.Core/Models/Terms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Gamedalf.Core.Infrastructure;
+
+namespace Gamedalf.Core.Models
+{
+    public class Terms : IDateCreatedTrackable
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [ScaffoldColumn(false)]
+        [ForeignKey("Employee")]
+        public string EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DateCreated { get; set; }
+    }
+}
