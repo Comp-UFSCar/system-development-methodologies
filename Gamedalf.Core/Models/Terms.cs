825205bcb3e8f6ab7010d737929f150bd37dd3a0
Lucas David <lucasolivdavid@gmail.com>
2014-10-15 21:50:34 -0300

    Update Terms

--- Gamedalf.Core/Models/Terms.cs
+++ Gamedalf.Core/Models/Terms.cs
@@ -8,7 +8,6 @@ namespace Gamedalf.Core.Models
     public class Terms : IDateCreatedTrackable
     {
         [Key]
-        [Column(Order = 1)]
         public int Id { get; set; }
 
         public string Title { get; set; }
