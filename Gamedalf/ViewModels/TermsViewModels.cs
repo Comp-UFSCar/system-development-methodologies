f71eb66e583e978fe9f68bf68f3399b07ff8718a
Lucas David <lucasolivdavid@gmail.com>
2014-10-18 09:41:03 -0300

    Create TermsJosnViewModel

--- Gamedalf/ViewModels/TermsViewModels.cs
+++ Gamedalf/ViewModels/TermsViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using Gamedalf.Core.Attributes;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Gamedalf.ViewModels
@@ -28,5 +29,16 @@ namespace Gamedalf.ViewModels
         [EnforceTrue]
         [Display(Name = "I hereby accept the presented terms")]
         public bool AcceptTerms { get; set; }
+    }
+
+    public class TermsJsonViewModel
+    {
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Content { get; set; }
+
+        public DateTime DateCreated { get; set; }
     }
 }
