2259ce50821c312ad33adb3d61768146f77766f9
Lucas David <lucasolivdavid@gmail.com>
2014-11-04 14:17:22 -0200

    Add TermsSeedData

---
+++ Gamedalf.Core/Migrations/TermsSeedData.cs
@@ -0,0 +1,23 @@
+﻿
+using Gamedalf.Core.Models;
+using System;
+using System.Collections.Generic;
+namespace Gamedalf.Core.Migrations
+{
+    class TermsSeedData
+    {
+        public ICollection<Terms> Data { get; set; }
+
+        public TermsSeedData()
+        {
+            Data = new List<Terms>
+            {
+                new Terms
+                {
+                    Title = "Developers",
+                }
+            };
+        }
+
+    }
+}
