2e1a06fad473a7aaa34392494fcb2d94eb299040
jvbrandaom <jvbrandaom@gmail.com>
2014-11-16 23:02:01 -0200

    PaymentService created

---
+++ Gamedalf.Services/PaymentService.cs
@@ -0,0 +1,30 @@
+﻿using Gamedalf.Core.Data;
+using Gamedalf.Core.Models;
+using Gamedalf.Services.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamedalf.Services
+{
+    public class PaymentService: Service<Payment>
+    {
+        public PaymentService(ApplicationDbContext db): base(db){}
+
+        public virtual async Task<ICollection<Payment>> Search(string q)
+        {
+            if (String.IsNullOrEmpty(q))
+            {
+                return await All();
+            }
+
+            return await Db.Payments
+                .Where(e => e.Player.UserName.Contains(q))
+                .OrderBy(e => e.Player)
+                .ToListAsync();
+        }
+
+    }
+}
