ba9d7f296fe30b34efe59cc7fc5111a3e4c8e211
jvbrandaom <jvbrandaom@gmail.com>
2014-11-17 14:39:33 -0200

    Added Subscription to DBContext

--- Gamedalf.Core/Data/ApplicationDbContext.cs
+++ Gamedalf.Core/Data/ApplicationDbContext.cs
@@ -54,6 +54,8 @@ namespace Gamedalf.Core.Data
         public virtual DbSet<Terms>     Terms      { get; set; }
         public virtual DbSet<Payment>   Payments    { get; set; }
 
+        public System.Data.Entity.DbSet<Gamedalf.Core.Models.Subscription> Subscriptions { get; set; }
+
         //public System.Data.Entity.DbSet<Gamedalf.Core.Models.Payment> Payments { get; set; }
     }
 }
