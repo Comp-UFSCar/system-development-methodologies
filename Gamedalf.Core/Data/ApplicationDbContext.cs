2da4d8990b742f6c52274c47f84b8fe2b9c2ca3d
jvbrandaom <jvbrandaom@gmail.com>
2014-11-16 23:01:33 -0200

    Payment included in DBContext

--- Gamedalf.Core/Data/ApplicationDbContext.cs
+++ Gamedalf.Core/Data/ApplicationDbContext.cs
@@ -52,5 +52,8 @@ namespace Gamedalf.Core.Data
         public virtual DbSet<Game>      Games      { get; set; }
         public virtual DbSet<Playing>   Playings   { get; set; }
         public virtual DbSet<Terms>     Terms      { get; set; }
+        public virtual DbSet<Payment>   Payments    { get; set; }
+
+        //public System.Data.Entity.DbSet<Gamedalf.Core.Models.Payment> Payments { get; set; }
     }
 }
