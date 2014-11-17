8ba4b6761440d5b997497f568e44507f0441d0eb
jvbrandaom <jvbrandaom@gmail.com>
2014-11-17 14:17:10 -0200

    Added SubscriptionService

---
+++ Gamedalf.Services/SubscriptionService.cs
@@ -0,0 +1,19 @@
+﻿using Gamedalf.Core.Models;
+using Gamedalf.Services.Infrastructure;
+using System.Threading.Tasks;
+using System.Linq;
+using Gamedalf.Core.Data;
+
+namespace Gamedalf.Services
+{
+    public class SubscriptionService : Service<Subscription>
+    {
+        public SubscriptionService(ApplicationDbContext db) : base(db){}
+
+        public virtual Subscription Last()
+        {
+            return  Db.Subscriptions
+                .Last();
+        }
+    }
+}
---
+++ Gamedalf/ViewModels/SubscriptionViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gamedalf.ViewModels
+{
+    public class SubscriptionCreateViewModel
+    {
+        [Required]
+        [Display(Name = "Subscription cost")]
+        public decimal Cost { get; set; }
+    }
+}
