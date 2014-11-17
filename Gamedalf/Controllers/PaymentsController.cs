436bf4278c681a411f284d57c0c2d8dc1fe50c09
jvbrandaom <jvbrandaom@gmail.com>
2014-11-16 23:02:51 -0200

    PaymentController created with Index implemented

---
+++ Gamedalf/Controllers/PaymentsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gamedalf.Core.Data;
+using Gamedalf.Core.Models;
+using System.Threading.Tasks;
+using Gamedalf.Services;
+
+namespace Gamedalf.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly PaymentService _payments;
+
+        /*
+        public PaymentsController(GameService games)
+        {
+            _games = games;
+        }*/
+
+        // GET: Payments
+        public async Task<ActionResult> Index(string q = null, int page = 1, int size = 10)
+        {
+            ViewBag.q = q;
+
+            var list = (await _payments.Search(q))
+                .ToPagedList(page, size);
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_List", list);
+            }
+
+            return View(list);
+        }
+
+
+    }
+}
