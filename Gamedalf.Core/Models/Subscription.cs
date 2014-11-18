449e3c006ecd541add101ac265d74b7690c14867
Lucas David <lucasolivdavid@gmail.com>
2014-11-17 23:32:34 -0200

    Timestamps Subscription

--- Gamedalf.Core/Models/Subscription.cs
+++ Gamedalf.Core/Models/Subscription.cs
@@ -1,14 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Gamedalf.Core.Infrastructure;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gamedalf.Core.Models
 {
-    public class Subscription
+    public class Subscription : IDateCreatedTrackable
     {
         [Key]
         public int Id { get; set; }
-        
+
         [Required]
         [Range(0, (double)decimal.MaxValue)]
         public decimal Cost { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DateCreated { get; set; }
     }
 }
